6241e58383d3912e3d68fd007905075ac725574b
tostilord <louis.voet@gmail.com>
2020-06-18 10:13:23 +0200

    update

---
+++ 03-thread.py
@@ -0,0 +1,16 @@
+import time
+import threading
+
+
+def myfunc(name):
+    print(f"myfunc started with {name}")
+    time.sleep(5)
+    print("myfunc ended")
+
+
+if __name__ == '__main__':
+    print("main started")
+    t = threading.Thread(target=myfunc, args=['Terrance'])
+    t.start()
+    myfunc("louis")
+    print("main ended")
---
+++ thread.py
@@ -0,0 +1,11 @@
+import time
+
+
+def myfunc():
+    print("Hello World!")
+    time.sleep(10)
+    return True
+
+
+if __name__ == '__main__':
+    myfunc()
